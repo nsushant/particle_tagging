825e1e46934cfa69fef9fde6e592595be240dab4
nsushant <64201587+nsushant@users.noreply.github.com>
2024-05-27 16:15:55 +0400

    Update __init__.py
    changes to correspond with directory structure

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
-from .spatial_tagging_module import *
-from .angular_momentum_tagging_module import *
+from .edge.spatial_tagging import *
+from .edge.angular_momentum_tagging import *
+from .tagging_functions.spatial_tagging import *
+from .tagging_functions.angular_momentum_tagging import *
 from .plotting import *
