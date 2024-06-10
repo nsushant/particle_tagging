856da0b1902a8f77880bc8bd1a39aa068b6d6f74
nsushant <64201587+nsushant@users.noreply.github.com>
2024-06-10 16:44:29 +0400

    Update tagging_function_def.py
    name changes in accordance with member files

--- tagging/tagging_function_def.py
+++ tagging/tagging_function_def.py
@@ -6,12 +6,12 @@ def tag_particles(DMOsim, path_to_particle_data = '/vol/ph/astro_data/shared/mor
     
     if tagging_method == 'angular momentum':
       
-      df_tagged = angt.tag_over_full_sim(DMOsim, fmb_percentage = free_param_val, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, mergers = include_mergers)
+      df_tagged = angt.angmom_tag_over_full_sim(DMOsim, fmb_percentage = free_param_val, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, mergers = include_mergers)
       
 
     if tagging_method == 'spatial' : 
 
-      df_tagged = spt.tag_over_full_sim(DMOsim, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, particle_storage_filename=None, mergers= include_mergers)
+      df_tagged = spt.spatial_tag_over_full_sim(DMOsim, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, particle_storage_filename=None, mergers= include_mergers)
 
 
     return df_tagged
