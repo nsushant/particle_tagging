fbdd58336ca933d741332ed05e2ea97eb7eb93e8
nsushant <64201587+nsushant@users.noreply.github.com>
2024-06-10 17:26:58 +0400

    Update __init__.py
    simplified imports

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
-from .edge.spatial_tagging import *
-from .edge.angular_momentum_tagging import *
-from .tagging.spatial_tagging import *
-from .tagging.angular_momentum_tagging import *
-from .tagging.utils import *
-from .analysis.plotting import *
-from .analysis.calculate import *
+from .edge import *
+
+from .tagging import *
+
+from .analysis import *
