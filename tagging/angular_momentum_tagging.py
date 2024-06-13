ae48afaf26f30605de992661d77ca93da422a732
nsushant <64201587+nsushant@users.noreply.github.com>
2024-06-13 16:42:04 +0400

    Update angular_momentum_tagging.py
    reff calculation function added

--- tagging/angular_momentum_tagging.py
+++ tagging/angular_momentum_tagging.py
@@ -530,7 +530,226 @@ def angmom_tag_over_full_sim(DMOsim, free_param_value = 0.01, pynbody_path  = '/
     return df_tagged_particles
 
 
+def angmom_calculate_reffs_over_full_sim(DMOsim, data_particles_tagged, pynbody_path  = '/vol/ph/astro_data/shared/morkney/EDGE/' , AHF_centers_file=None):
+
+    '''
+
+    Given a tangos simulation, the function performs angular momentum based tagging over the full simulation. 
+
+    Inputs: 
+
+    DMOsim - tangos simulation 
+    pynbody_path - path to particle data 
+    data_particles_tagged - dataframe containing tagged particle data (tagged mstar, particle IDs, tagging times)
     
+    Returns: 
+    
+    dataframe with half-mass radii calculated using tagged particles. 
+    
+    '''
+
+    
+    
+    pynbody.config["halo-class-priority"] = [pynbody.halo.hop.HOPCatalogue]
+                    
+    sims = [str(sim_name)]
+
+    DMOname = DMOsim.path
+    
+    t_all, red_all, main_halo,halonums,outputs = load_indexing_data(DMOsim,1)
+    print(outputs)
+    
+
+    #load in the two files containing the particle data
+    if ( len(red_all) != len(outputs) ) : 
+        print('output array length does not match redshift and time arrays')
+ 
+
+    data_t = np.asarray(data_particles_tagged['t'].values)
+    
+    stored_reff = np.array([])
+    stored_reff_acc = np.array([])
+    stored_reff_z = np.array([])
+    stored_time = np.array([])
+    kravtsov_r = np.array([])
+    stored_reff_tot = np.array([])
+    KE_energy = np.array([])
+    PE_energy = np.array([])
+
+    AHF_centers = pd.read_csv(str(AHF_centers_file)) if AHF_centers_supplied == True else None
+            
+    for i in range(len(outputs)):
+
+        gc.collect()
+
+        
+        if len(np.where(data_t <= float(t_all[i]))) == 0:
+            continue
+
+        
+        dt_all = data_particles_tagged[data_particles_tagged['t']<=t_all[i]]
+
+        data_grouped = dt_all.groupby(['iords']).last()
+
+        selected_iords_tot = data_grouped.index.values
+
+        data_insitu = data_grouped[data_grouped['type'] == 'insitu']
+        
+        selected_iords_insitu_only = data_insitu.index.values
+        
+        if selected_iords_tot.shape[0]==0:
+            continue
+        
+        mstars_at_current_time = data_grouped['mstar'].values
+        
+        half_mass = float(mstars_at_current_time.sum())/2
+        
+        print(half_mass)
+        
+        #get the main halo object at the given timestep if its not available then inform the user.
+
+       
+        hDMO = tangos.get_halo(DMOname+'/'+outputs[i]+'/halo_'+str(halonums[i]))
+            
+        print(hDMO)
+            
+        #for  the given path,entry,snapshot at given index generate a string that includes them
+        simfn = join(pynbody_path,DMOname,outputs[i])
+        
+        # try to load in the data from this snapshot
+        try:  DMOparticles = pynbody.load(simfn)
+
+        # where this data isn't available, notify the user.
+        except:
+            print('--> DMO particle data exists but failed to read it, skipping!')
+            continue
+        
+        # once the data from the snapshot has been loaded, .physical_units()
+        # converts all array’s units to be consistent with the distance, velocity, mass basis units specified.
+        DMOparticles.physical_units()
+
+        
+
+        try:
+            if AHF_centers_file==None:
+                h = DMOparticles.halos()[int(halonums[i])-1]
+                
+            elif AHF_centers_file != None:
+                pynbody.config["halo-class-priority"] = [pynbody.halo.ahf.AHFCatalogue]
+                
+                
+                AHF_crossref = AHF_centers[AHF_centers['i'] == i]['AHF catalogue id'].values[0]
+                    
+                h = DMOparticles.halos()[int(AHF_crossref)] 
+                        
+                children_ahf = AHF_centers[AHF_centers['i'] == i]['children'].values[0]
+                        
+                child_str_l = children_ahf[0][1:-1].split()
+
+                children_ahf_int = list(map(float, child_str_l))
+
+                    
+                #pynbody.analysis.halo.center(h)
+                    
+                #pynbody.config["halo-class-priority"] = [pynbody.halo.hop.HOPCatalogue]
+                
+                
+                halo_catalogue = DMOparticles.halos()
+                
+                subhalo_iords = np.array([])
+                    
+                for i in children_ahf_int:
+                            
+                    subhalo_iords = np.append(subhalo_iords,halo_catalogue[int(i)].dm['iord'])
+                                                                                                                                             
+                h = h[np.logical_not(np.isin(h['iord'],subhalo_iords))] if len(subhalo_iords) >0 else h
+                
+
+                
+            pynbody.analysis.halo.center(h)
+            #pynbody.config["halo-class-priority"] = [pynbody.halo.hop.HOPCatalogue]
+
+        except:
+            print('centering data unavailable')
+            continue
+
+
+        try:
+            r200c_pyn = pynbody.analysis.halo.virial_radius(h.d, overden=200, r_max=None, rho_def='critical')
+
+        except:
+            print('could not calculate R200c')
+            continue
+        DMOparticles = DMOparticles[sqrt(DMOparticles['pos'][:,0]**2 + DMOparticles['pos'][:,1]**2 + DMOparticles['pos'][:,2]**2) <= r200c_pyn ]
+        
+        particle_selection_reff_tot = DMOparticles[np.isin(DMOparticles['iord'],selected_iords_tot)] if len(selected_iords_tot)>0 else []
+
+        particles_only_insitu = DMOparticles[np.isin(DMOparticles['iord'],selected_iords_insitu_only)] if len(selected_iords_insitu_only) > 0 else []
+
+        
+        if (len(particle_selection_reff_tot))==0:
+            print('skipped!')
+            continue
+        else:
+
+            dfnew = data_particles_tagged[data_particles_tagged['t']<=t_all[i]].groupby(['iords']).last()
+    
+            masses = [dfnew.loc[n]['mstar'] for n in particle_selection_reff_tot['iord']]
+
+            masses_insitu = [data_insitu.loc[iord]['mstar'] for iord in particles_only_insitu['iord']]
+                
+            cen_stars = calc_3D_cm(particles_only_insitu,masses_insitu)
+            
+            particle_selection_reff_tot['pos'] -= cen_stars
+            
+            masses = [dfnew.loc[n]['mstar'] for n in particle_selection_reff_tot['iord']]
+
+            #particle_selection_reff_tot['pos'] -= cen_stars 
+
+            distances =  np.sqrt(particle_selection_reff_tot['x']**2 + particle_selection_reff_tot['y']**2 + particle_selection_reff_tot['z']**2)
+
+            #caculate the center of mass using all the tagged particles
+            #cen_of_mass = center_on_tagged(distances,masses)
+            
+                        
+            idxs_distances_sorted = np.argsort(distances)
+
+            sorted_distances = np.sort(distances)
+
+            distance_ordered_iords = np.asarray(particle_selection_reff_tot['iord'][idxs_distances_sorted])
+            
+            print('array lengths',len(set(distance_ordered_iords)),len(distance_ordered_iords))
+
+            sorted_massess = [dfnew.loc[n]['mstar'] for n in distance_ordered_iords]
+            
+            cumilative_sum = np.cumsum(sorted_massess)
+
+            R_half = sorted_distances[np.where(cumilative_sum >= (cumilative_sum[-1]/2))[0][0]]
+            #print(cumilative_sum)
+            
+            halfmass_radius = []
+
+            stored_reff_z = np.append(stored_reff_z,red_all[i])
+            stored_time = np.append(stored_time, t_all[i])
+               
+            stored_reff = np.append(stored_reff,float(R_half))
+            kravtsov = hDMO['r200c']*0.02
+            kravtsov_r = np.append(kravtsov_r,kravtsov)
+
+            particle_selection_reff_tot['pos'] += cen_stars
+
+            print('halfmass radius:',R_half)
+            print('Kravtsov_radius:',kravtsov)
+            
+        
+  
+
+    print('---------------------------------------------------------------writing output file --------------------------------------------------------------------')
+
+    df_reff = pd.DataFrame({'reff':stored_reff,'z':stored_reff_z, 't':stored_time,'kravtsov':kravtsov_r})
+    
+    
+    return df_reff
 
     
     
