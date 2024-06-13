2048aee5d14de96cee98d01f157fc575d741c640
nsushant <64201587+nsushant@users.noreply.github.com>
2024-06-13 21:36:22 +0400

    Update tagging_function_def.py
    name changes

--- tagging/tagging_function_def.py
+++ tagging/tagging_function_def.py
@@ -2,16 +2,16 @@ from .spatial_tagging import *
 from .angular_momentum_tagging import *
 
 
-def tag_particles(DMOsim, path_to_particle_data = '/vol/ph/astro_data/shared/morkney/EDGE/', tagging_method = 'angular momentum', free_param_val = 0.01, include_mergers = True, darklight_occupation_frac = 'all' ):
+def tag_particles(DMO_database, path_to_particle_data = '/vol/ph/astro_data/shared/morkney/EDGE/1459_DMO', tagging_method = 'angular momentum', free_param_val = 0.01, include_mergers = True, darklight_occupation_frac = 'all' ):
     
     if tagging_method == 'angular momentum':
       
-      df_tagged = angmom_tag_over_full_sim(DMOsim, free_param_value = free_param_val, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, mergers = include_mergers)
+      df_tagged = angmom_tag_over_full_sim(DMO_database, free_param_value = free_param_val, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, mergers = include_mergers)
       
 
     if tagging_method == 'spatial' : 
 
-      df_tagged = spatial_tag_over_full_sim(DMOsim, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, particle_storage_filename=None, mergers= include_mergers)
+      df_tagged = spatial_tag_over_full_sim(DMO_database, pynbody_path  = path_to_particle_data, occupation_frac = darklight_occupation_frac, particle_storage_filename=None, mergers= include_mergers)
 
 
     return df_tagged
