42ed028944185d97fa98bc0b82f6ef21088fb540
nsushant <64201587+nsushant@users.noreply.github.com>
2025-01-07 16:33:41 +0400

    Update angular_momentum_tagging_HYDRO_ARRAYS.py
    Added Padding with zeros for the hydro mstar array in the recursive tagging function.

--- tagging/angular_momentum_tagging_HYDRO_ARRAYS.py
+++ tagging/angular_momentum_tagging_HYDRO_ARRAYS.py
@@ -649,7 +649,11 @@ def angmom_tag_over_full_sim_recursive(DMOsim,tstep, halonumber, free_param_valu
     # check time and output array have same size 
     if ( len(red_all) != len(outputs) ) : 
         print('output array length does not match redshift and time arrays')
-    
+        
+    if len(outputs) != len(mstar_s_insitu):
+        mstar_s_insitu = np.pad(mstar_s_insitu,(len(outputs)-len(mstar_s_insitu),0))
+
+
     # group_mergers groups all merging halo objects by redshift.
     hmerge_added, z_set_vals = group_mergers(zmerge,hmerge)
     
